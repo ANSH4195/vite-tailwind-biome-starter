1ff3333dda9b6b85570609ac55175c85f4f7075e
Ansh Shrivastava <shri.ansh4563@gmail.com>
2024-06-22 21:22:49 +0100

    Make it generic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
 	return (
-		<h1 className="text-3xl font-bold underline">I'm gonna be carousel</h1>
+		<h1 className="text-3xl font-bold underline">
+			What is it gonna be this time?
+		</h1>
 	);
 }
 
